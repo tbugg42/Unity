5e1991fb283759941d8a1cebae843f56795b067d
Thomas Bugg <10738754@CS518-22.ad.uvu.edu>
2015-08-31 16:16:25 -0600

    Added script

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{   
+	int myInt = 5;
+
+	void Start ()
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+	
+	
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
